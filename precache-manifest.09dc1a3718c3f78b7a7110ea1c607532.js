f7ed20a7442c5cd3c59ec94469b50fde57a5c6b8
revortni <rojit.blaze13@gmail.com>
2020-02-26 14:37:34 +0545

    Updates

---
+++ precache-manifest.09dc1a3718c3f78b7a7110ea1c607532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0115f64deac71609b7890b1f57bde9",
+    "url": "/signoidsass/index.html"
+  },
+  {
+    "revision": "77366c4820706182a894",
+    "url": "/signoidsass/static/css/main.c6442669.chunk.css"
+  },
+  {
+    "revision": "a94ed2e7d3946b1a9cbb",
+    "url": "/signoidsass/static/js/2.0ce2fe87.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/signoidsass/static/js/2.0ce2fe87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77366c4820706182a894",
+    "url": "/signoidsass/static/js/main.822e33a1.chunk.js"
+  },
+  {
+    "revision": "d4e10494f65984e02eb9",
+    "url": "/signoidsass/static/js/runtime-main.16c29646.js"
+  }
+]);
