57371758eea32b1d476a7ebcd8b6b02e39906e18
revortni <rojit.blaze13@gmail.com>
2020-02-28 11:03:05 +0545

    added active class using NavLink

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   let history = useHistory();
 
   return (
     <nav className='nav'>
-      <Link to='/projects' alt='nav' className='nav__link nav__link--active'>
+      <NavLink
+        to='/projects'
+        alt='nav'
+        className='nav__link '
+        activeClassName='nav__link--active'
+      >
         projects
-      </Link>
-      <Link to='/datasets' alt='nav' className='nav__link'>
+      </NavLink>
+      <NavLink
+        to='/datasets'
+        alt='nav'
+        className='nav__link'
+        activeClassName='nav__link--active'
+      >
         data library
-      </Link>
-      <Link
+      </NavLink>
+      <NavLink
         to='#insight-gallery'
         alt='nav'
         className='nav__link nav__link--disabled'
       >
         insight gallery
-      </Link>
+      </NavLink>
     </nav>
   );
 };
