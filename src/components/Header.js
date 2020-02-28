7f7a09f7d259609edf91842c5e65684103c63eee
revortni <rojit.blaze13@gmail.com>
2020-02-28 10:45:37 +0545

    added responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
 import Nav from './Nav';
 import AvatarImg from '../assets/pp.png';
+import { MdMenu } from 'react-icons/md';
 
 const Header = params => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleStyle = 'header__toggle-container--show';
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header className='header'>
       <div className='container'>
@@ -11,19 +18,28 @@ const Header = params => {
           <Logo />
         </div>
         <div className='header__options'>
-          <Nav />
-          <div className='header__profile'>
-            <a href='#profile'>
-              <div className='profile avatar'>
-                <div className='profile__image avatar__photo'>
-                  <img src={AvatarImg} alt='profile' />
+          <div
+            className={
+              'header__toggle-container ' + (showMenu ? toggleStyle : '')
+            }
+          >
+            <Nav />
+            <div className='header__item'>
+              <a href='#profile'>
+                <div className='profile avatar'>
+                  <div className='profile__image avatar__photo'>
+                    <img src={AvatarImg} alt='profile' />
+                  </div>
+                  <span className='profile__info'>john</span>
                 </div>
-                <span className='profile__info'>john</span>
-              </div>
-            </a>
-            <span className='icon'>
-              <i className='fa fa-sort-down'></i>
-            </span>
+              </a>
+              <span className='icon'>
+                <i className='fa fa-sort-down'></i>
+              </span>
+            </div>
+          </div>
+          <div className='header__toggle-icon' onClick={toggleMenu}>
+            <MdMenu size={24} />
           </div>
         </div>
       </div>
